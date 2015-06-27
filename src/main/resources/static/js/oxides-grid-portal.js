f8b272e3d3cc12a01beb799eeec3b305a4a04b06
Rafal Kluszczynski <rkluszczynski@gmail.com>
2015-06-27 15:08:10 +0200

    first example of static page with working angularjs

---
+++ src/main/resources/static/js/oxides-grid-portal.js
@@ -0,0 +1,17 @@
+var oxidesGridPortalApp = angular.module('oxidesGridPortal', [
+    'ngRoute',
+    'ui.bootstrap'
+]);
+
+oxidesGridPortalApp.controller('oxidesGridPortalController', function ($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+
+  $scope.yourName = null;
+});
