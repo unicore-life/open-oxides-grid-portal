16f916147b47ecd3cf579f2c01d6b9fe82981753
Rafal Kluszczynski <rkluszczynski@gmail.com>
2015-07-10 03:08:40 +0200

    showing jobs uris in simple table

--- src/main/java/pl/edu/icm/oxides/AngularTestingController.java
+++ src/main/java/pl/edu/icm/oxides/AngularTestingController.java
@@ -2,23 +2,34 @@ package pl.edu.icm.oxides;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.icm.oxides.simulation.model.OxidesSimulation;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 @RestController
 public class AngularTestingController {
 
-    @RequestMapping(value = "/testing", method = RequestMethod.GET)
+    @RequestMapping(value = "/testing", method = GET)
     public List<String> getItemList() {
         return Arrays.asList("Test1", "Test2", "Test3");
     }
 
-    @RequestMapping(value = "/unauthorized", method = RequestMethod.GET)
+    @RequestMapping(value = "/unauthorized", method = GET)
     public ResponseEntity<String> entityResponse() {
         return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+
+    @RequestMapping(value = "/testing-post", method = POST,
+            consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public OxidesSimulation submitSimulation(@RequestBody OxidesSimulation simulation) {
+        return new OxidesSimulation("DONE");
     }
 }
--- src/main/resources/public/assets/js/oxides-grid-portal.js
+++ src/main/resources/public/assets/js/oxides-grid-portal.js
@@ -4,6 +4,7 @@ var oxidesGridPortalApp = angular.module('oxidesGridPortal', [
     'ui.bootstrap'
 ]);
 
+
 oxidesGridPortalApp.controller('oxidesGridPortalController', function ($scope) {
   $scope.phones = [
     {'name': 'Nexus S',
@@ -16,3 +17,32 @@ oxidesGridPortalApp.controller('oxidesGridPortalController', function ($scope) {
 
   $scope.yourName = null;
 });
+
+oxidesGridPortalApp.controller('oxidesSimulationsListingController',
+    function ($scope, oxidesSimulationsListingService, modelSimulationsListing) {
+        $scope.simulations = modelSimulationsListing;
+
+        oxidesSimulationsListingService.getJson()
+            .success(function(data, status, headers, config) {
+                angular.copy(data, modelSimulationsListing);
+            })
+            .error(function(data, status, headers, config) {
+                alert('Failed: HTTP Status Code = ' + status);
+            });
+    }
+);
+
+
+oxidesGridPortalApp.factory('oxidesSimulationsListingService',
+    ['$http', function ($http) {
+        return {
+            getJson: function () {
+                return $http.get('/oxides/unicore-jobs');
+            }
+        };
+    }]
+);
+
+
+oxidesGridPortalApp.value('modelSimulationsListing', []);
+
