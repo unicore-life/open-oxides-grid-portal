37f881e28c806f602a829450a4a19aa22972a7d1
Rafal Kluszczynski <rkluszczynski@gmail.com>
2015-07-31 23:51:14 +0200

    upload testing

--- src/main/java/pl/edu/icm/oxides/unicore/GridFileUploader.java
+++ src/main/java/pl/edu/icm/oxides/unicore/GridFileUploader.java
@@ -1,6 +1,8 @@
 package pl.edu.icm.oxides.unicore;
 
 import eu.unicore.util.httpclient.IClientConfiguration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,7 +24,11 @@ public class GridFileUploader {
     public String uploadFileToGrid(MultipartFile file, String uri, AuthenticationSession authenticationSession) {
         IClientConfiguration clientConfiguration = clientHelper.createClientConfiguration(authenticationSession);
 
-        String name = file.getName();
+        String fileName = file.getName();
+        String originalFilename = file.getOriginalFilename();
+        log.info(String.format("Storing file with name '%s' and original filename '%s'", fileName, originalFilename));
+
+        String name = originalFilename;
         if (!file.isEmpty()) {
             try {
                 byte[] bytes = file.getBytes();
@@ -38,4 +44,6 @@ public class GridFileUploader {
             return "You failed to upload " + name + " because the file was empty.";
         }
     }
+
+    private Log log = LogFactory.getLog(GridFileUploader.class);
 }
