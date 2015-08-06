4574900075a290d0ca2eb3d1d38c2433023af47e
Rafal Kluszczynski <rkluszczynski@gmail.com>
2015-08-06 14:19:31 +0200

    workaround for re-upload file with select

--- src/main/resources/public/assets/js/testing-angular-upload-file.js
+++ src/main/resources/public/assets/js/testing-angular-upload-file.js
@@ -2,35 +2,39 @@ var myApp = angular.module('myApp', [
     'angularFileUpload'
 ]);
 
+
 myApp.controller('uploadController', function ($scope, FileUploader) {
+    $scope.destinationUri = 'TODO';
 
     $scope.uploader = new FileUploader({
         url: '/oxides/unicore/upload',
         alias: 'uploadFile',
         queueLimit: 1,
-        formData: [{
-            destinationUri: 'TODO'
-        }],
+//        formData: [{
+//            destinationUri: $scope.destinationUri
+//        }],
         removeAfterUpload: true,
 
-        //onBeforeUploadItem: function (item) {
-        //    Array.prototype.push.apply(item.formData, [{
-        //        destinationUri: 'TODO',
-        //    }]);
-        //},
+        onBeforeUploadItem: function (item) {
+            Array.prototype.push.apply(item.formData, [{
+                destinationUri: $scope.destinationUri,
+            }]);
+        },
 
         onSuccessItem: function (item, response, status, headers) {
-            console.log('SUCCESS');
+            console.info('SUCCESS');
         },
 
         onCompleteItem: function (item, response, status, headers) {
             this.clearQueue();
+            // Workaround for re-upload:
+            $scope.uploader._directives.select[0].element[0].value = '';
             console.log('STATUS: ' + status);
         },
     });
 
-    //$scope.uploader.FileSelect.prototype.isEmptyAfterSelection = function () {
-    //    return false; // true|false
-    //};
+//    $scope.uploader.FileSelect.prototype.isEmptyAfterSelection = function () {
+//        return true; // true|false
+//    };
 });
 
